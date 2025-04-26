26e1f696f501a5fc39a30adf8287e638bec33d4a
Daniel Wang <103014684+cloudyapple4@users.noreply.github.com>
2025-04-26 21:50:12 +1000

    disable quick tap term on layer keys

--- config/config.h
+++ config/config.h
@@ -4,6 +4,7 @@
 #define TAPPING_TERM 200
 #define PERMISSIVE_HOLD
 #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+#define QUICK_TAP_TERM_PER_KEY
 
 // Combos
 #define COMBO_TERM 20
--- config/keymap.c
+++ config/keymap.c
@@ -58,6 +58,16 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     }
 };
 
+uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LT(LOWER, KC_ENT):
+        case LT(RAISE, KC_TAB):
+            return 0;
+        default:
+            return QUICK_TAP_TERM;
+    }
+}
+
 // Combos
 enum combos {
     EU_LPRN,
